a492458140d9c317e9fd6e3102fce786b9f54ec5
Angel Bulnes <angel_bulnes16@yahoo.com>
2019-03-05 21:42:06 -0600

    Agregado codigo AMI y ADI

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import messagebox
 
 
 class Interfaz:
@@ -11,11 +12,13 @@ class Interfaz:
         self.alto = self.v.winfo_height()
         self.v.title('Proyecto II - Redes de Datos II')
         self.v.geometry('500x500')
-        
+
         self.initComponents()
+
+    def show(self):
         self.v.mainloop()
-    
-    
+
+
     def initComponents(self):
         self.v.grid_columnconfigure(1, weight = 1)
         self.titulo = Label(self.v, text= 'Graficador de codigos de linea')
@@ -29,27 +32,124 @@ class Interfaz:
         self.lb3 = Label(self.v, text = 'Codigo de linea')
         self.lb3.grid(row = 2, column = 0, padx =(30,10), pady = (5,5))
         self.comboLineas = ttk.Combobox(self.v, state = 'readonly')
-        self.comboLineas['values'] = ['NRZ','RZ','AMI','ADI','B3ZS','HDB3','B6ZS','Manchester','CMI']
+        self.comboLineas['values'] = ['NRZ','RZ','AMI','ADI','B3ZS(+v)','B3ZS(-v)','HDB3(+v)','HDB3(-v)','B6ZS(+v)','B6ZS(-v)','Manchester','CMI']
         self.comboLineas.configure(width = 22)
         self.comboLineas.grid(row = 2, column = 1, padx = (10,10), pady =(5,5))
-        self.crearGrafico()
+        self.comboLineas.bind("<<ComboboxSelected>>", self.selecciona)
         self.addMenu()
-        
+        #self.iniciarCanvas()
+
 
     def addMenu(self):
         self.barra = Menu(self.v)
         self.barra.add_command(label = 'Salir',command = self.v.destroy)
         self.barra.add_command(label = 'Integrantes', command = self.mostrarI)
         self.v.config(menu = self.barra)
+
     
-    def crearGrafico(self):
+
+    def crearGrafico(self,val,cod):
+        X = []
+        Y = []
         f = Figure(figsize=(4,2.5),dpi = 100)
         a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7],[4,5,2,1,7,9,10])
+        if val == 'NRZ':
+            for i in range(len(cod)):
+                X.append(i)
+                X.append(i+1)
+                Y.append(cod[i])
+                Y.append(cod[i])
+            a.plot(X,Y)
+        if val == 'RZ':
+            for i in range(len(cod)):
+                if cod[i] == 1:
+                    X.append(i)
+                    X.append(i+0.5)
+                    X.append(i+0.5)
+                    X.append(i+1)
+                    Y.append(1)
+                    Y.append(1)
+                    Y.append(0)
+                    Y.append(0)
+                else:
+                    X.append(i)
+                    X.append(i+1)
+                    Y.append(0)
+                    Y.append(0)
+            a.plot(X,Y)
+        if val == 'AMI':
+            polaridad = 1
+            for i in range(len(cod)):
+                if cod[i] == 1: 
+                    if polaridad == 1:
+                        X.append(i)
+                        X.append(i+0.5)
+                        X.append(i+0.5)
+                        X.append(i+1)
+                        Y.append(1)
+                        Y.append(1)
+                        Y.append(0)
+                        Y.append(0)
+                        polaridad = -1
+                    else:
+                        X.append(i)
+                        X.append(i+0.5)
+                        X.append(i+0.5)
+                        X.append(i+1)
+                        Y.append(-1)
+                        Y.append(-1)
+                        Y.append(0)
+                        Y.append(0)
+                        polaridad = 1 
+                else:
+                    X.append(i)
+                    X.append(i+1)
+                    Y.append(0)
+                    Y.append(0) 
+            a.plot(X,Y)
+        if val == 'ADI':
+            for i in range(len(cod)):
+                if i % 2 != 0 and i != 0: 
+                    if (cod[i] == 1):
+                        X.append(i)
+                        X.append(i+1)
+                        Y.append(0)
+                        Y.append(0)
+                    else:
+                        X.append(i)
+                        X.append(i+1)
+                        Y.append(1)
+                        Y.append(1) 
+                else:
+                    X.append(i)
+                    X.append(i+1)
+                    Y.append(cod[i])
+                    Y.append(cod[i])        
+            a.plot(X,Y)
+                 
         canvas = FigureCanvasTkAgg(f,master = self.v)
         canvas.get_tk_widget().grid(row = 3, column = 0, columnspan = 2,pady = (40,40))
-        
-        
+
+
+    def selecciona(self, event):
+        validos = (0,1)
+        cod = []
+        val = self.comboLineas.get()
+        aux = self.txt.get()
+        aux = list(aux)
+        for i in range(len(aux)):
+            cod.append(int(aux[i]))
+            if cod[i] not in validos:
+                messagebox.showerror(message="Cadena de bits invalida", title="Error")
+                self.txt.delete(0, END)
+                self.comboLineas.set('')
+                self.txt.focus()
+                error= True
+                val= 'empty'
+                cod=[]
+                break
+        self.crearGrafico(val,cod)
+
     def mostrarI(self):
         nombres = ['Maverick Martinez ', 'Luis Irias','Mario Zavala', 'Luis Andino', 'Angel Bulnes']
         num_cuenta = ['20141001825','2015100','20161003851','20151003836','20161000007']
@@ -63,7 +163,7 @@ class Interfaz:
         self.frame.grid(row = 0, column = 0, padx = (ancho_a*0.02, ancho_a*0.02), pady = (alto_a*0.05,alto_a*0.05))
         self.titulo = Label(self.frame,text = 'Integrantes')
         self.titulo.grid(row = 0, column = 0, columnspan = 2)
-       
+
         i = 0
         for n in nombres:
             self.lbN = Label(self.frame, text = n)
@@ -75,5 +175,6 @@ class Interfaz:
             self.lbNum.grid(row = i+1, column = 1, padx = (5,5), pady = (2,2))
             i+=1
 
-
-v = Interfaz()
+if __name__ == "__main__":
+    v = Interfaz()
+    v.show()
